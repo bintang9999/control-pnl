b2e364a01ba57ac4e5e4614abbdfaa6db0380a9e
bintang9999 <ramadhanib967@gmail.com>
2026-05-29 17:03:01 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true,
+    proxy: {
+      '/api': 'http://127.0.0.1:3001',
+      '/socket.io': {
+        target: 'http://127.0.0.1:3001',
+        ws: true
+      }
+    }
+  }
 })
